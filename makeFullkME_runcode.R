24d89c0e0e0bccd818a6abc9c6e93d206aa39c5f
reliscu <rebeliscu@gmail.com>
2021-11-25 11:21:41 -0800

    Add serialized version of function

--- makeFullkME_runcode.R
+++ makeFullkME_runcode.R
@@ -1,10 +1,11 @@
 source("/home/rebecca/omicon/code/make_kme/makeFullkME_fxn.R")
 library(tictoc) ## Calc. time for function to run
+library(data.table)
 
 setwd("/home/rebecca/omicon/code/make_kme")
-datExpr <- as.data.frame(fread("Make_full_kME_table/Sun_GSE4290_glioma_154_ComBat.csv"))
-modEig <- as.data.frame(fread("Make_full_kME_table/Module_eigengenes_18-12-19.csv"))
-kme <- as.data.frame(fread("Make_full_kME_table/kME_table_18-12-19.csv"))
+datExpr <- as.data.frame(fread("Sun_GSE4290_glioma_154_ComBat.csv"))
+modEig <- as.data.frame(fread("Module_eigengenes_18-12-19.csv"))
+kme <- as.data.frame(fread("kME_table_18-12-19.csv"))
 nThreads <- 15
 
 tic()
