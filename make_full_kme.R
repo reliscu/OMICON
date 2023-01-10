a0004fcf26c6fe9831c3cab89bbaade5eb5777f5
reliscu <rebeliscu@gmail.com>
2023-01-09 17:34:11 -0800

    fix bug in instance of just 1 module

--- make_full_kme.R
+++ make_full_kme.R
@@ -1,7 +1,9 @@
+setwd("~/omicon/transformations/make_full_kME")
+
 library(data.table)
 library(tictoc)
 
-source("~/make_full_kme_fxns.R")
+source("make_full_kme_fxns.R")
 
 analyte_data <- fread("data/E-MTAB-3892_Tumor_169_ComBat.csv", data.table=F)
 module_eig <- fread("data/Module_eigengenes_08-39-41.csv", data.table=F)
@@ -10,7 +12,7 @@ n_threads <- 15
 parallel <- T
 
 cat("\n")
-cat("Calculating kME for all module eigengenes.\nThis may take a while.")
+cat("Calculating kME for all module eigengenes.\nThis may take a while for large networks.")
 
 tic()
 full_kme <- make_full_kme(analyte_data, module_eig, kme_partial, parallel, n_threads)
